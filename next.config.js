d7cee6267f59c10f99ffa73cda2f70eff29b6dda
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-16 16:38:14 +0200

    Reorganize code structure

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  },
+};
